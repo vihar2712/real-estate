a8affefc88fbfb2b432332a1b2898b25d20579c5
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-06 15:42:38 +0530

    added routes and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500">Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="p-3 bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto">
+        <Link to="/">
+          <h1>
+            <span className="text-slate-500 font-bold text-xl">Vihar</span>
+            <span className="text-slate-700 font-bold text-xl">Estate</span>
+          </h1>
+        </Link>
+        <form className="flex items-center p-3 bg-slate-100 rounded-md">
+          <input
+            type="text"
+            className="bg-transparent focus:outline-none w-64"
+            placeholder="Search..."
+          />
+          <FaSearch className="hover:cursor-pointer" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hover:underline ">Home</li>
+          </Link>
+          <Link to="/about">
+            {" "}
+            <li className="hover:underline ">About</li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="hover:underline ">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
