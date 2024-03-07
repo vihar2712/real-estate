52b8153d23a1c65d2c23e7f4613774039856bc7e
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-07 11:04:13 +0530

    mongodb integrated for database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("database connection established");
+  })
+  .catch((err) => {
+    console.log("error connecting to database", err);
+  });
 
 const app = express();
 
