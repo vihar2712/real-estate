60aff26d9008894ef0fc6434354622e49d8eeadf
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-07 20:53:59 +0530

    sign in api route implemented

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,4 +15,19 @@ export const signup = async (req, res, next) => {
     next(err);
     // next(errorHandler(555,'custom error using error handler function'));
   }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  const validUser = await User.findOne({ email });
+  if (!validUser) return next(errorHandler(401, "user not found"));
+  const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+  const validPassword = bcryptjs.compareSync(password, validUser.password);
+  if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+  const { password: pass, ...restInfo } = validUser._doc;
+
+  res
+    .cookie("access_token", token, { httpOnly: true })
+    .status(200)
+    .json(restInfo);
 };
