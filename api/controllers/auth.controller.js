49f6db3c1e587b2cbb95d30aee6e8632a4e2e05e
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-07 13:41:19 +0530

    added middleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password);
   const newUser = new User({ username, email, password: hashPassword });
@@ -10,6 +11,7 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.status(201).send("user created successfully");
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    next(err);
+    // next(errorHandler(555,'custom error using error handler function'));
   }
 };
