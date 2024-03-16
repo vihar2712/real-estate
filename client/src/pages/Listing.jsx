2f27aaa341637365aae4a120b58c46bc5dad030c
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-16 22:23:00 +0530

    added details in listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,6 +5,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkerAlt,
+  FaParking,
+} from "react-icons/fa";
 
 const Listing = () => {
   SwiperCore.use(Navigation);
@@ -24,19 +31,80 @@ const Listing = () => {
   return (
     <div>
       {userListing ? (
-        <Swiper navigation>
-          {userListing.imageUrls.map((url) => (
-            <SwiperSlide key={url}>
-              <div
-                className="h-[500px]"
-                style={{
-                  background: `url(${url}) center no-repeat`,
-                  backgroundSize: "cover",
-                }}
-              ></div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
+        <div>
+          <Swiper navigation>
+            {userListing.imageUrls.map((url) => (
+              <SwiperSlide key={url}>
+                <div
+                  className="h-[400px]"
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className="w-6/12 mx-auto flex flex-col gap-2">
+            <h1 className="text-lg font-semibold my-6">
+              {userListing.title}
+              <span>
+                {" "}
+                - ${" "}
+                {userListing.discountPrice > 0
+                  ? userListing.discountPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  : userListing.regularPrice
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                {userListing.type === "sale" ? "" : "/ month"}
+              </span>
+            </h1>
+            <span className="flex items-center gap-2">
+              <FaMapMarkerAlt className="text-orange-700" />
+              {userListing.address}
+            </span>
+            <div className="flex gap-6">
+              <span className="bg-orange-900 text-white py-2 px-10 rounded-md">
+                For {userListing.type}
+              </span>
+              {userListing.discountPrice > 0 && (
+                <span className="bg-green-900 text-white py-2 px-10 rounded-md">
+                  $
+                  {(+userListing.regularPrice - +userListing.discountPrice)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+                  discount
+                </span>
+              )}
+            </div>
+            <p className="text-slate-700">
+              <span className="font-semibold text-black">Description - </span>
+              {userListing.description}
+            </p>
+            <div className="flex gap-6 flex-wrap">
+              <span className="flex gap-1 items-center">
+                <FaBed className="text-orange-700" />
+                {userListing.bedrooms}
+                {userListing.bedrooms > 1 ? " beds" : "bed"}
+              </span>
+              <span className="flex gap-1 items-center">
+                <FaBath className="text-orange-700" />
+                {userListing.bathrooms}
+                {userListing.bathrooms > 1 ? " baths" : " bath"}
+              </span>
+              <span className="flex gap-1 items-center">
+                <FaParking className="text-orange-700" />
+                {userListing.parking ? "Parking Spot" : "No parking"}
+              </span>
+              <span className="flex gap-1 items-center">
+                <FaChair className="text-orange-700" />
+                {userListing.furnished ? "Furnished" : "Not Furnished"}
+              </span>
+            </div>
+          </div>
+        </div>
       ) : (
         <Loading />
       )}
