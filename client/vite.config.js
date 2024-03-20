ad0e47fab68686ee899530237be2e13d47225295
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-21 01:12:43 +0530

    vercel backend link added

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://vercel.com/vihar2712s-projects/real-estate-backend/DbSBxcpp41bNLG5khivn6rdfYvGc",
         secure: false,
       },
     },
