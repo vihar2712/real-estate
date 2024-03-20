5ddf0e08ae05a79835f274f2c2b8fd081b229831
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-20 22:46:16 +0530

    vercel dependent code added

--- api/index.js
+++ api/index.js
@@ -33,10 +33,10 @@ app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listingRoute);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/public]")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 app.use((err, req, res, next) => {
