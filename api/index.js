f5d1202ad9f5f9d42645d9e9d53586d66e2cc0d9
viharshah27 <Vihar.shah@asambhav.org.in>
2024-03-20 22:56:27 +0530

    changes for deployment

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listingRoute);
 
-app.use(express.static(path.join(__dirname, "/client/dist]")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
