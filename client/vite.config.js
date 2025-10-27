6fb89162a0e85d3527d6d486cf21c7f2c480fd17
vihar shah <viharshah222@gmail.com>
2025-10-27 19:23:41 -0400

    added netlify.toml file for redirect

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ mode }) => {
-  // Load env variables
-  const env = loadEnv(mode, process.cwd(), "");
-
-  // Choose backend based on mode
-  const backendUrl =
-    mode === "development"
-      ? "http://localhost:3010"
-      : "https://real-estate-backend-31n7.onrender.com";
-
-  return {
-    server: {
-      proxy: {
-        "/api": {
-          target: backendUrl,
-          changeOrigin: true,
-          secure: false,
-        },
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3010",
+        secure: false,
       },
     },
-    define: {
-      __APP_BACKEND_URL__: JSON.stringify(backendUrl),
-    },
-    plugins: [react()],
-  };
+  },
+  plugins: [react()],
 });
